05f1612e65696ac6406b11e89514e173ec56a8c3
mkozachek <mkozachek@gmail.com>
2015-02-11 16:34:51 -0500

    Basic Event Handler Implemented
    Events can be subscribed to and published. Unsubscribing from events
    still needs to be added w/ a binary search, as handlers get sorted as
    they come in by raw pointer value.
    
    TODO: Implement testing code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,38 @@
+use std::rc::Rc;
+use std::cmp::Ordering;
+
 enum Event<E> {
     Args(E),
     Missing,
 }
 
-struct EventHandler<F)> {
-    e: &Event,
-    handlers: &mut Vec<F>,
+// To deal with handler functions - F: Rc<Box<Fn(&event<E>)>>
+struct EventPublisher<E> {
+    handlers: Vec<Rc<Box<Fn(&Event<E>) + 'static>>>,
+}
+
+impl<E> EventPublisher<E> {
+
+    pub fn new() -> EventPublisher<E> {
+        EventPublisher{ 
+            handlers: Vec::<Rc<Box<Fn(&Event<E>) + 'static>>>::new() 
+        }
+    }
+
+    pub fn subscribe_handler(&mut self, handler_rc: Rc<Box<Fn(&Event<E>) + 'static>>){
+        self.handlers.push(handler_rc.clone());
+        self.handlers.sort_by(|a,b| (&**a as *const _).cmp(&(&**b as *const _)))
+    }
+    /// - Implement this when I've got the rest of it working. Use raw pointer comparison and a binary search
+    /*pub fn remove_handler<E,F>(&mut self, handler: Box<F>){
+        
+    }*/
+    
+    pub fn publish_event(&self, event: &Event<E>){
+        for handler in self.handlers.iter(){
+            handler(event);
+        }
+    }
 }
 
 #[test]
