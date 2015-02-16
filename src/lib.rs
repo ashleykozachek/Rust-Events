7dc79c3875ff7549b834e7f6fec5440723a774ec
mkozachek <mkozachek@gmail.com>
2015-02-16 18:33:50 -0500

    Started multithreaded development.
    Because of how much has to change, this is going to be a separate fork
    to try to implement multithreading.

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,46 @@
-#![crate_id = "event"]
-#![crate_type = "rlib"]
 #![allow(dead_code)]
 
 use std::rc::Rc;
 use std::cmp::Ordering;
+use std::sync::Arc;
+use std::thread::{Thread, JoinGuard};
 
 /// Enumerator of the Event type. Whatever type E of Event::Args you implement here is the type E that will be used for the EventPublisher.
-pub enum Event<E> {
+pub enum Event<E: Send + Sync> {
     Args(E),
     Missing,
 }
 
-// To deal with handler functions - F: Rc<Box<Fn(&event<E>)>>
+// To deal with handler functions - F: Rc<Box<Fn(&event<E: Send + Sync>)>>
 /// EventPublisher. Works similarly to C#'s event publishing pattern. Event handling functions are subscribed to the publisher.
 /// Whenever the publisher fires an event it calls all subscribed event handler functions.
-/// Use event::EventPublisher::<E>::new() to construct
-pub struct EventPublisher<E> {
-    handlers: Vec<Rc<Box<Fn(&Event<E>) + 'static>>>,
+/// Use event::EventPublisher::<E: Send + Sync>::new() to construct
+pub struct EventPublisher<E: Send + Sync> {
+    handlers: Vec<Arc<Box<Fn(&Event<E>) + Send + Sync>>>,
 }
 
-impl<E> EventPublisher<E> {
+impl<E> EventPublisher<E> where E: Send + Sync{
 
     /// Event publisher constructor.
     pub fn new() -> EventPublisher<E> {
         EventPublisher{ 
-            handlers: Vec::<Rc<Box<Fn(&Event<E>) + 'static>>>::new() 
+            handlers: Vec::<Arc<Box<Fn(&Event<E>) + Send + Sync>>>::new() 
         }
     }
     /// Subscribes event handler functions to the EventPublisher.
-    /// INPUT:  handler_box: Box<Fn(&Event<E>) + 'static>>   handler_box is a box pointer to a function to handle an event of the type E. The function must
+    /// INPUT:  handler_box: Box<Fn(&Event<E: Send + Sync>) + Send + Sync>>   handler_box is a box pointer to a function to handle an event of the type E. The function must
     ///     be capable of handling references to the event type set up by the publisher, rather than the raw event itself.
     /// OUTPUT: void
-    pub fn subscribe_handler(&mut self, handler_box: Box<Fn(&Event<E>) + 'static>){
+    pub fn subscribe_handler(&mut self, handler_box: Box<Fn(&Event<E>) + Send + Sync>){
 
-        self.handlers.push( Rc::new(handler_box) );
+        self.handlers.push( Arc::new(handler_box) );
         self.handlers.sort_by(|a,b| (&**a as *const _).cmp(&(&**b as *const _))) 
     }
     
     /// Unsubscribes an event handler from the publisher.
-    /// INPUT:  handler_box: Box<Fn(&Event<E>) + 'static>    handler_box is a box pointer to a function to handle an event of the type E.
+    /// INPUT:  handler_box: Box<Fn(&Event<E: Send + Sync>) + Send + Sync>    handler_box is a box pointer to a function to handle an event of the type E.
     /// OUTPUT: bool    output is a bool of whether or not the function was found in the list of subscribed event handlers and subsequently removed.
-    pub fn unsubscribe_handler(&mut self, handler_box: Box<Fn(&Event<E>) + 'static>) -> bool {
+    pub fn unsubscribe_handler(&mut self, handler_box: Box<Fn(&Event<E>) + Send + Sync>) -> bool {
     
         let len = self.handlers.len();
         
@@ -57,7 +57,7 @@ impl<E> EventPublisher<E> {
     ///         mid: usize              Middle of the current binary search boundaries.
     ///         u_bound: usize          Upper bound of the binary search indecies.
     /// OUTPUT: bool                    True/False as to whether or not the event handler function was found and removed from the list.
-    fn unsub_common_match(&mut self, handler_box: Box<Fn(&Event<E>) + 'static>, l_bound: usize, mid: usize, u_bound: usize) -> bool {
+    fn unsub_common_match(&mut self, handler_box: Box<Fn(&Event<E>) + Send + Sync>, l_bound: usize, mid: usize, u_bound: usize) -> bool {
         let p_handler = &*handler_box as *const _;
         match (p_handler as usize).cmp(&(&**self.handlers[mid] as *const _ as usize)){
             Ordering::Less => {
@@ -79,7 +79,7 @@ impl<E> EventPublisher<E> {
     ///         mid: usize              Middle of the current binary search boundaries.
     ///         u_bound: usize          Upper bound of the binary search indecies.
     /// OUTPUT: bool                    True/False as to whether or not the event handler function was found and removed from the list.
-    fn recursive_unsub_search(&mut self, handler_box: Box<Fn(&Event<E>) + 'static>, l_bound: usize, u_bound: usize) -> bool {
+    fn recursive_unsub_search(&mut self, handler_box: Box<Fn(&Event<E>) + Send + Sync>, l_bound: usize, u_bound: usize) -> bool {
         let p_handler = &*handler_box as *const _;
         if l_bound == u_bound{
             if p_handler == (&**self.handlers[l_bound] as *const _){
@@ -93,11 +93,22 @@ impl<E> EventPublisher<E> {
     }
     
     // TODO: Implement this concurrently
-    /// Publishes events, pushing the &Event<E> to all handler functions stored by the event publisher.
-    /// INPUT: event: &Event<E>     Reference to the Event<E> being pushed to all handling functions.
+    /// Publishes events, pushing the &Event<E: Send + Sync> to all handler functions stored by the event publisher.
+    /// INPUT: event: &Event<E: Send + Sync>     Reference to the Event<E: Send + Sync> being pushed to all handling functions.
     pub fn publish_event(&self, event: &Event<E>){
         for handler in self.handlers.iter(){
             handler(event);
+        }
+    }
+    
+    pub fn publish_event_multithreaded(&self, event: &Event<E>){
+        let shared_event = Arc::new(event);
+        let guards = Vec::<JoinGuard<_>>::new();
+        
+        for handler in self.handlers{
+            //let cloned_handler = handler.clone();
+            let cloned_event = shared_event.clone();
+            guards.push(Thread::scoped(move || {handler(&cloned_event)}));
         }
     }
 }
