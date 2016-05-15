7b64a87cd3445233a6a95caa8901feee21f10be7
mkozachek <mkozachek@gmail.com>
2016-05-15 02:05:55 -0700

    Updated to Rust 1.8.0 Syntax
    Rust 1.8 has significantly streamlined the use of function pointers
    compared to 1.0 nightly. Admittedly this was a year ago. As such, I've
    updated this library to use modern function pointers and raw pointers.
    
    Yo Dawg, I heard you like pointers, so I pointer in your pointer so you
    can dereference while you point.
    
    Basically converting to a raw pointer for a function pointer results in
    a reference to a reference. Thus, you need to dereference the first
    reference. It's a bit clunky and that's why there's an unsafe block to
    deal with this.
    
    Please note that even though the raw pointer dereferencing happens, this
    is *only* to gain the pointer value of the function and use it for
    sorting into the binary tree storing handlers. No pointer arithmetic is
    done nor any crazy typecasting.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![allow(dead_code)]
 
-use std::rc::Rc;
 use std::collections::BTreeMap;
 
 /// Enumerator of the Event type. Whatever type E of Event::Args you implement here is the type E that will be used for the EventPublisher.
@@ -15,7 +14,7 @@ pub enum Event<E> {
 /// Use event::EventPublisher::<E>::new() to construct
 pub struct EventPublisher<E> {
     //handlers: Vec<Rc<Box<Fn(&Event<E>) + 'static>>>,
-    handlers: BTreeMap<usize, Rc<Box<Fn(&Event<E>) + 'static>>>,
+    handlers: BTreeMap<usize, fn(&Event<E>)>,
 }
 
 impl<E> EventPublisher<E> {
@@ -23,29 +22,30 @@ impl<E> EventPublisher<E> {
     /// Event publisher constructor.
     pub fn new() -> EventPublisher<E> {
         EventPublisher{ 
-            //handlers: Vec::<Rc<Box<Fn(&Event<E>) + 'static>>>::new() 
-            handlers: BTreeMap::<usize, Rc<Box<Fn(&Event<E>) + 'static>>>::new()
+            handlers: BTreeMap::<usize, fn(&Event<E>)>::new()
         }
     }
     /// Subscribes event handler functions to the EventPublisher.
-    /// INPUT:  handler_box: Box<Fn(&Event<E>) + 'static>>   handler_box is a box pointer to a function to handle an event of the type E. The function must
+    /// INPUT:  handler: fn(&Event<E>) handler is a pointer to a function to handle an event of the type E. The function must
     ///     be capable of handling references to the event type set up by the publisher, rather than the raw event itself.
     /// OUTPUT: void
-    pub fn subscribe_handler(&mut self, handler_box: Box<Fn(&Event<E>) + 'static>){
-
-        //self.handlers.push( Rc::new(handler_box) );
-        //self.handlers.sort_by(|a,b| (&**a as *const _).cmp(&(&**b as *const _))) 
-        let p_handler = &*handler_box as *const _;
-        
-        self.handlers.insert(p_handler as usize, Rc::new(handler_box));
+    pub fn subscribe_handler(&mut self, handler: fn(&Event<E>)){
+        let p_handler: usize;
+        unsafe{
+            p_handler = *(handler as *const usize);
+        }
+        self.handlers.insert(p_handler, handler);
     }
     
     /// Unsubscribes an event handler from the publisher.
-    /// INPUT:  handler_box: Box<Fn(&Event<E>) + 'static>    handler_box is a box pointer to a function to handle an event of the type E.
+    /// INPUT:  handler: fn(&Event<E>) handler is a pointer to a function to handle an event of the type E.
     /// OUTPUT: bool    output is a bool of whether or not the function was found in the list of subscribed event handlers and subsequently removed.
-    pub fn unsubscribe_handler(&mut self, handler_box: Box<Fn(&Event<E>) + 'static>) -> bool {
-        let p_handler = &*handler_box as *const _;
-        match self.handlers.remove(&(p_handler as usize)){
+    pub fn unsubscribe_handler(&mut self, handler: fn(&Event<E>)) -> bool {
+        let p_handler: usize;
+        unsafe{
+            p_handler = *(handler as *const usize);
+        }
+        match self.handlers.remove(&p_handler){
             Some(_) => true,
             None => false,
         }
